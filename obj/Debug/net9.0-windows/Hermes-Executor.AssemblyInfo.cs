cac672a2e665eb2a1bb7f9e7b1a3ec95dd99040b
Dema Adzhani <demaadzhani08@gmail.com>
2026-08-30 13:18:29 +0700

    v1

--- obj/Debug/net9.0-windows/Hermes-Executor.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows/Hermes-Executor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Hermes Roblox Executor - Premium UI")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c698ec1351bf0f1d6d609f3169b19b5073308fa8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hermes Executor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hermes Executor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
